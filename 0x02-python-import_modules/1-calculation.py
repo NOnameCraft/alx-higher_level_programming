03a88397fd57d28debfd82339733f02334046ecc
Stylhaut <nonamecraft01@gmail.com>
2022-07-11 17:32:54 +0000

    import & modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,6 +3,6 @@ from calculator.py import add,sub,mul,div
 a = 10
 b = 15
 print("{:d} + {:d} = {:d}".format(a,b,add(a,b))
+print("{:d} - {:d} = {:d}".format(a,b,sub(a,b))
 print("{:d} * {:d} = {:d}".format(a,b,mul(a,b))
-print("{:d} / {:d} = {:d}".format(a,b,div(a,b))
-print("{:d} - {:d} = {:d}".format(a,b,sub(a,b)
+print("{:d} / {:d} = {:d}".format(a,b,div(a,b)
